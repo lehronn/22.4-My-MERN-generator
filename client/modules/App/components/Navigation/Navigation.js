1b9f5b65d9a774f315cc1a30cee356712337870d
lehronn <lehronn@gmail.com>
2017-12-18 07:07:34 +0100

    navigation is work

--- client/modules/App/components/Navigation/Navigation.js
+++ client/modules/App/components/Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { FormattedMessage } from 'react-intl';
 
 const Navigation = (props) => (
   <div>
     <ul>
-      <li>Home</li>
-      <li>Posts</li>
-      <li>About</li>
+      <li><Link to="/home" ><FormattedMessage id="home" /></Link></li>
+      <li><Link to="/" ><FormattedMessage id="posts" /></Link></li>
+      <li><Link to="/about" ><FormattedMessage id="about" /></Link></li>
     </ul>
   </div>
 );
